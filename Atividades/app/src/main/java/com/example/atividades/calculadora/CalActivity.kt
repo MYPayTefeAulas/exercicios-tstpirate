4342723116b8a89072ecb8e402966c93f879e1c2
Thalia de Souza Teixeira <tst.fis17@uea.edu.br>
2021-11-06 12:21:11 -0400

    att cal

--- Atividades/app/src/main/java/com/example/atividades/calculadora/CalActivity.kt
+++ Atividades/app/src/main/java/com/example/atividades/calculadora/CalActivity.kt
@@ -2,6 +2,7 @@ package com.example.atividades.calculadora
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import com.example.atividades.databinding.ActivityCalBinding
@@ -11,20 +12,39 @@ class CalActivity : AppCompatActivity() {
 
 
 //    private val calculadora = Calculadora()
-    private var equacao: TextView? = null
+    private var equacao: EditText? = null
     private var resultado: TextView? = null
+    private var backSpace: Button? = null
+    private var limpar: Button? = null
+    private var parenteses: Button? = null
+    private var porcentagem: Button? = null
+    private var divisao: Button? = null
+    private var sete: Button? = null
+    private var oito: Button? = null
+    private var nove: Button? = null
+    private var produto: Button? = null
+    private var quatro: Button? = null
+    private var cinco: Button? = null
+    private var seis: Button? = null
+    private var subtracao: Button? = null
+    private var um: Button? = null
+    private var dois: Button? = null
+    private var tres: Button? = null
+    private var soma: Button? = null
+    private var expoente: Button? = null
+    private var zero: Button? = null
+    private var ponto: Button? = null
+    private var igual: Button? = null
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityCalBinding.inflate(layoutInflater)
 
-        var operacao: String = ""
-        var resultado: String = ""
 
         binding.btLimpar.setOnClickListener {
-            operacao = ""
-            resultado = ""
+//            operacao = ""
+//          resultado = ""
 //            binding.txtOperacao.text = ""
         }
 
